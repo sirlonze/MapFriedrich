44d6140ff091bf7fac21453139ee6b34a015fe13
sirlonze <78441718+sirlonze@users.noreply.github.com>
2021-02-09 19:34:01 +0100

    Version 1 Test

---
+++ !modern-edits-doors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="!modern-edits-doors" tilewidth="32" tileheight="32" tilecount="192" columns="12">
+ <image source="Tilesets/!modern-edits-doors.png" width="384" height="512"/>
+</tileset>
